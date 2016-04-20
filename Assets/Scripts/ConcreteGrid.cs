592b148c001f1726d81f0c905212244cd6749ba8
José Ángel Pastrana Padilla <japp0005@red.ujaen.es>
2016-04-20 22:13:06 +0200

    Genial. Estructura de datos del plano.

---
+++ Assets/Scripts/ConcreteGrid.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+using Grid;
+
+public class ConcreteGrid : NetworkBehaviour {
+
+	// Estados de una celda de la malla.
+	public enum Cell {
+		FREE_P1 = 0, // La casilla está libre y puede ser ocupada por el jugador 1.
+		FREE_P2 = 1, // La casilla está libre y puede ser ocupada por el jugador 2.
+		OWNER_GAME = 2, // La casilla está bloqueada. No pertenece a ningún jugador.
+		OWNER_P1 = 3, // La casilla está ocupada y pertenece al jugador 1.
+		OWNER_P2 = 4, // La casilla está ocupada y pertenece al jugador 2.
+	};
+
+	// Estructura de datos que almacena el estado de las casillas.
+	[SyncVar]
+	RegularGrid grid;
+
+	// Valores por defecto del script.
+	public const double dxmin = 0, dymin = 0, dxmax = 100, dymax = 100;
+	public const uint dcols = 1, drows = 1;
+
+	// Valores concretos de las dimensiones de la superficie que cubre la malla.
+	public double xmin = dxmin, ymin = dymin, xmax = dxmax, ymax = dymax;
+	public uint cols = dcols, rows = drows;
+
+	/**
+	 * Devuelve la celda contenida para el punto de superficie dado.
+	 * Si el punto sobresale la malla, se devuelve Cell.OWNER_GAME
+	 */
+	public Cell getCell(double x, double y) {
+		if (grid.isContained(x, y)) {
+			return (Cell) grid.getPoint (x, y);
+		}
+		return Cell.OWNER_GAME;
+	}
+
+	/**
+	 * Establece el valor de una celda para el punto de superficie dado.
+	 * Si el punto sobresale de la malla, no hace nada.
+	 */
+	public void setCell(double x, double y, int cell) {
+		if (grid.isContained (x, y)) {
+			grid.setPoint (x, y, (int) cell);
+		}
+	}
+
+	/**
+	 * Establece la casilla con propiedad para el juego.
+	 */
+	public void setCellOwnerGame(double x, double y) {
+		setCell (x, y, (int) Cell.OWNER_GAME);
+	}
+
+	/**
+	 * Establece la casilla con propiedad para el jugador (identificado por su id)
+	 */
+	public void setCellOwnerPlayer(double x, double y, int player) {
+		setCell(x, y, (int) (Cell.FREE_P1 + player)); 
+	}
+
+	/**
+	 * El jugador ocupa una casilla si está libre y le pertenece.
+	 * Devuelve False si no se pudo cometer la acción.
+	 */
+	public bool setCellOwnerPlayerIfFree(double x, double y) {
+		Cell cell = getCell (x, y);
+		if (cell < Cell.OWNER_GAME) {
+			setCell (x, y, (int) (cell + 2));
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * El jugador libera una casilla que haya ocupado.
+	 * Devuelve False si no se pudo cometer la acción.
+	 */
+	public bool setCellFreePlayerIfTaken(double x, double y) {
+		Cell cell = getCell (x, y);
+		if (cell > Cell.OWNER_GAME) {
+			setCell (x, y, (int) (cell - 2));
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Instanciación de las casillas del juego.
+	 */
+	void Start() {
+		grid = new RegularGrid(cols, rows, xmin, ymin, xmax, ymax);
+
+		for (uint col = 0; col < cols; ++col) {
+			for (uint row = 0; row < rows; ++row) {
+				grid.setCell (col, row, (int) Cell.OWNER_GAME);
+			}
+		}
+	}
+}
--- Assets/Scripts/RegularGrid.cs
+++ Assets/Scripts/RegularGrid.cs
@@ -3,70 +3,132 @@
  * Discretiza el espacio continuo en una matriz cuyas celdas tienen una dimensión concreta.
  */
 
-/*public static class RegularGrid<T> {
+using System;
+using UnityEditor;
+using UnityEngine;
 
-	/** Divisiones en 2D de la malla. */
-	//private int cols, rows; // cols = x, rows = y
+namespace Grid {
+	[Serializable]
+	public class RegularGrid  {
 
-	/** Matriz de tipo T que almacena un valor para la casilla normalizada (x, y) */
-	//private T[,] grid;
+		/** Divisiones en 2D de la malla. */
+		[SerializeField]
+		private uint cols, rows; // cols = x, rows = y
 
-	/** Magnitud de una casilla. */
-	//private double width, height;
+		/** Matriz de tipo int que almacena un valor para la casilla normalizada (x, y) */
+		[SerializeField]
+		private int[,] grid;
 
-	/** Límites de la superficie de la malla. */
-	//private double xmin, ymin, xmax, ymax;
-//
-	/**
-	 * Constructor de una malla regular.
-	 * 
-	 * @param cols Número de divisiones en la componente X.
-	 * @param rows Número de divisiones en la componente Y.
-	 * @param xmin Componente mínima en X de la superficie.
-	 * @param xmax Componente máxima en X de la superficie.
-	 * @param ymin Componente mínima en Y de la superficie.
-	 * @param ymax Componente máxima en Y de la superficie.
-	 */
-	/*RegularGrid(uint _x, uint _y, double _xmin, double _ymin, double _xmax, double _ymax) {
-	/	cols   = _x;
-		rows   = _y;
-		grid   = new T[_x, _y];
-		width  = (_xmax - _xmin) / _x;
-		height = (_ymax - _ymin) / _y;
-		xmin   = _xmin;
-		ymin   = _ymin;
-		xmax   = _xmax;
-		ymax   = _ymax;
-	}*/
+		/** Magnitud de una casilla. */
+		[SerializeField]
+		private double width, height;
 
-	/**
-	 * Determina si un punto de superficie está dentro de la malla.
-	 * 
-	 * @param x Componente X de la superficie.
-	 * @param y Componente Y de la superficie.
-	 */
-/*private bool isContained(double x, double y) {
-		return (((xmin <= x) && (x < xmax)) && ((ymin <= x) && (y < ymax)));
-	}
-*/
-	/**
-	 * Normaliza los valores de un punto de superficie a una casilla de la malla.
-	 * Cuando la normalización no es posible (porque el punto sobresalga de la superficie), devuelve False.
-	 * 
-	 * @param x Parámetro de entrada. Componente X de la superficie
-	 * @param y Parámetro de entrada. Componente Y de la superficie
-	 * @param c Parámetro de salida.  Columna de la malla.
-	 * @param r Parámetro de salida.  Fila de la malla.
-	 */
-/*	private bool normalized(double x, double y, ref int c, ref int r) {
-		if (isContained (x, y)) {
-			c = (int)(((x - xmin) / width) * cols);
-			r = (int)(((y - ymin) / height) * rows);
-			return true;
+		/** Límites de la superficie de la malla. */
+		[SerializeField]
+		private double xmin, ymin, xmax, ymax;
+
+		/**
+		 * Constructor de una malla regular.
+		 * 
+		 * @param x Número de divisiones en la componente X.
+		 * @param y Número de divisiones en la componente Y.
+		 * @param xmin Componente mínima en X de la superficie.
+		 * @param xmax Componente máxima en X de la superficie.
+		 * @param ymin Componente mínima en Y de la superficie.
+		 * @param ymax Componente máxima en Y de la superficie.
+		 */
+		public RegularGrid(uint _x, uint _y, double _xmin, double _ymin, double _xmax, double _ymax) {
+			cols   = _x;
+			rows   = _y;
+			grid   = new int[_x, _y];
+			width  = (_xmax - _xmin) / _x;
+			height = (_ymax - _ymin) / _y;
+			xmin   = _xmin;
+			ymin   = _ymin;
+			xmax   = _xmax;
+			ymax   = _ymax;
 		}
-		return false;
+
+		/**
+		 * Normaliza los valores de un punto de superficie a una casilla de la malla.
+		 * 
+		 * @param x Parámetro de entrada. Componente X de la superficie.
+		 * @param y Parámetro de entrada. Componente Y de la superficie.
+		 * @param c Parámetro de salida.  Columna de la malla.
+		 * @param r Parámetro de salida.  Fila de la malla.
+		 */
+		private void normalized(double x, double y, out uint c, out uint r) {
+			c = (uint) ((x - xmin) / width);
+			r = (uint) ((y - ymin) / height);
+		}
+
+		/**
+		 * Determina si un punto de superficie está dentro de la malla.
+		 * 
+		 * @param x Componente X de la superficie.
+		 * @param y Componente Y de la superficie.
+		 */
+		public bool isContained(double x, double y) {
+			return (((xmin <= x) && (x < xmax)) && ((ymin <= x) && (y < ymax)));
+		}
+
+		/**
+		 * Dado un punto de superficie, establece su valor.
+		 * 
+		 * @param x Componente X de la superficie.
+		 * @param y Componente Y de la superficie.
+		 * @param data Valor que será establecido en la casilla que ocupa el punto.
+		 */
+		public void setPoint(double x, double y, int data) {
+			uint c, r;
+			normalized(x, y, out c, out r);
+			grid [c, r] = data;
+		}
+
+		/**
+		 * Devuelve el valor almacenado en la casilla para un punto de la superficie.
+		 * 
+		 * @param x Componente X de la superficie.
+		 * @param y Componente Y de la superficie.
+		 */
+		public int getPoint(double x, double y) {
+			uint c, r;
+			normalized (x, y, out c, out r);
+			return grid [c, r];
+		}
+
+		/**
+		 * Devuelve el número de columnas (eje de abscisas, eje X) de la malla almacenada.
+		 */
+		public uint getCols() {
+			return cols;
+		}
+
+		/**
+		 * Devuelve el número de filas (eje de ordenadas, eje Y) de la malla almacenada.
+		 */
+		public uint getRows() {
+			return rows;
+		}
+
+		/**
+		 * Devuelve el valor almacenado en una celda.
+		 * 
+		 * @param col Columna de la celda.
+		 * @para row Fila de la celda.
+		 */
+		public int getCell(uint col, uint row) {
+			return grid [col, row];
+		}
+
+		/**
+		 * Establece el valor para una celda.
+		 * 
+		 * @param col Columna de la celda.
+		 * @para row Fila de la celda.
+		 */
+		public void setCell(uint col, uint row, int data) {
+			grid [col, row] = data;
+		}
 	}
-
-
 }
-*/
