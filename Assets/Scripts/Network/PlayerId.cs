b959f5ec8bef1f6c1297cf0b85c97a17beaa6d3b
José Ángel Pastrana Padilla <japp0005@red.ujaen.es>
2016-04-24 19:08:09 +0200

    Vida y dinero simplificados en PlayerId

--- Assets/Scripts/Network/PlayerId.cs
+++ Assets/Scripts/Network/PlayerId.cs
@@ -4,38 +4,117 @@ using System.Collections;
 
 public class PlayerId : NetworkBehaviour {
 
-	[SyncVar]
+	static bool DEBUG = true;
+
+	// ATRIBUTOS DE UN JUGADOR:
+	// - GameObject de él mismo.
+	// - Identificador dentro del juego asignado por NetworkMan.
+	// - Color asignado por NetworkMan.
+	// - Vida al inicio.
+	// - Dinero al inicio.
+
+	[SyncVar(hook = "hook_id")]
 	int id;
 
-	[SyncVar]
+	[SyncVar(hook = "hook_color")]
 	Color c;
 
-	public void Start(){
+	[SyncVar(hook = "hook_health")]
+	public int health = 150;
+
+	[SyncVar(hook = "hook_money")]
+	public int money = 100;
+
+	// Asignación de la cámara al inicio de la partida.
+	// Para la asignación, se considera el Id del jugador.
+	public void Start()
+	{
 		if (!isLocalPlayer)
 			return;
-		// Asignación de cámaras.
-		print("Identificador:" + id);
-		if (id == 0) {
+
+		if (id == 0)
+		{
 			Camera.main.transform.position = new Vector3 (18.0f, 30.0f, -24.0f);
 			Camera.main.GetComponent<freeCam>().updateCamera (false);
 			GetComponent<Flicking> ().swapMaterial (true); 
-		} else if (id == 1) {
+		} 
+		else if (id == 1) 
+		{
 			Camera.main.transform.position = new Vector3 (82.0f, 30.0f, 124.0f);
 			Camera.main.GetComponent<freeCam>().updateCamera (true);
 			GetComponent<Flicking> ().swapMaterial (false); 
-		} 
+		}
 	}
 
-	public void setAttributes(int _id, Color _c) {
+	// Establece los atributos principales del jugador: id y color.
+	public void setAttributes(int _id, Color _c)
+	{
 		id = _id;
 		c = _c;
 	}
 
-	public int getId() {
+	// Devuelve la Id asignada al jugador por NetworkMan.
+	public int getId()
+	{
 		return id;
 	}
 
-	public Color getColor() {
+	// Devuelve el color asignado al jugador por NetworkMan.
+	public Color getColor()
+	{
 		return c;
+	}
+
+	// EJECUCIÓN EN EL SERVIDOR.
+	// El jugador recibe una cantidad de daño.
+	// Si la cantidad (amount) es positiva, pierde vida.
+	// Si la cantidad (amount) es negativa, gana vida.
+	public void TakeDamage(int amount)
+	{
+		if (!isServer)
+			return;
+
+		health -= amount;
+	}
+
+	// EJECUCIÓN EN EL SERVIDOR.
+	// Indica si el jugador está muerto. (Su vida es igual o inferior a 0).
+	public bool IsDead()
+	{
+		if (!isServer)
+			return false;
+
+		return (health <= 0);
+	}
+
+	// EJECUCIÓN EN EL SERVIDOR.
+	// El jugador incrementa la cantidad de dinero que almacena en cierta cantidad (amount).
+	public void GainMoney(int amount)
+	{
+		if (!isServer)
+			return;
+
+		money += amount;
+	}
+
+	// HOOKS...
+	void hook_id(int _id) {
+		if (DEBUG)
+			print ("Id actual: " + id);
+	}
+
+	void hook_color(Color _c) {
+		if (DEBUG)
+			print ("Color actual: " + _c);
+	}
+
+	void hook_health(int _health) {
+		if (DEBUG)
+			print ("Vida actual: " + _health);
+	}
+
+	void hook_money(int _money) {
+		if (DEBUG)
+			print ("Dinero actual: " + _money);
 	}
 }
--- Assets/Scripts/Network/TowerSpawner.cs
+++ Assets/Scripts/Network/TowerSpawner.cs
@@ -1,48 +1,96 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.Networking;
+using UnityEngine.Networking.NetworkSystem;
 
 public class TowerSpawner : NetworkBehaviour {
 
 	public GameObject tower;
 
-	[ClientCallback]
-	void Update(){
+	enum TowerCase {
+		PushConfirm = 0,
+		PopConfirm = 1,
+		RejectOut = 2,
+		RejectMoney = 3
+	};
+
+
+	[ClientRpc]
+	void RpcTowerBase(TowerCase tc)
+	{
 		if (!isLocalPlayer)
 			return;
+		
+		switch (tc) 
+		{
+			case TowerCase.PushConfirm:
+				print ("Torre colocada.");
+				break;
+			case TowerCase.PopConfirm:
+				print ("Torre eliminada.");
+				break;
+			case TowerCase.RejectOut:
+				print ("No se puede construír aquí.");
+				break;
+			case TowerCase.RejectMoney:
+				print ("No tienes suficiente dinero.");
+				break;
+			default:
+				break;
+		};
+	}
+
+	[ClientCallback]
+	void Update()
+	{
+		if (!isLocalPlayer)
+			return;
+
 		if (Input.GetKeyDown (KeyCode.Mouse0)) {
-			Camera coffee = Camera.main;
-			Ray vRay = coffee.ScreenPointToRay (Input.mousePosition);
+			Ray vRay = Camera.main.ScreenPointToRay (Input.mousePosition);
 			RaycastHit hit = new RaycastHit ();
 			Physics.Raycast (vRay, out hit, 1000);
-			GameObject goc = hit.collider.gameObject;
-			if ((!goc.tag.Equals("casilla_j1")) && (!goc.tag.Equals("casilla_j2")))
+			GameObject collider = hit.collider.gameObject;
+
+			PlayerId playerId = gameObject.GetComponent<PlayerId> ();
+
+			if (((playerId.getId () == 0) && (!collider.tag.Equals ("casilla_j1"))) || ((playerId.getId () == 1) && (!collider.tag.Equals ("casilla_j2"))))
+			{
+				RpcTowerBase (TowerCase.RejectOut);
 				return;
-			PlayerId id = gameObject.GetComponent<PlayerId> ();
-			if ((id.getId() == 0) && (!goc.tag.Equals ("casilla_j1")))
-				return;
-			if ((id.getId() == 1) && (!goc.tag.Equals ("casilla_j2")))
-				return;
-			CmdSpawn (hit.point, id.getColor(), gameObject, goc); // Lo llaman los clientes!
-			// goc.SetActive(false);
+			}
+
+			CmdSpawn (hit.point, playerId.gameObject, collider); // Lo llaman los clientes!
 		}
 	}
 
 	// Lo ejecuta el servidor!
 	[Command]
-	void CmdSpawn(Vector3 point, Color c, GameObject gop, GameObject goc) {
-		Money m = gop.GetComponent<Money> ();
-		Health h = gop.GetComponent<Health> ();
-		TowerInfo ti = tower.GetComponent<TowerInfo> ();
-		if (m.currentMoney >= ti.cost) {
-			GameObject instance = (GameObject)Instantiate (tower, goc.transform.position, goc.transform.rotation);
-			instance.GetComponent<SyncTowerBase> ().setTowerBase (goc).deactivate ();
-			instance.GetComponent<SyncOwner> ().setOwner (gop);
+	void CmdSpawn(Vector3 point, GameObject player, GameObject collider)
+	{
+		PlayerId playerId = player.GetComponent<PlayerId> ();
+		TowerInfo towerInfo = tower.GetComponent<TowerInfo> ();
+		TowerCase msg;
+
+		if (playerId.money >= towerInfo.cost) {
+			GameObject instance = (GameObject)Instantiate (tower, collider.transform.position, collider.transform.rotation);
+
+			instance.GetComponent<SyncTowerBase> ().setTowerBase (collider).deactivate ();
+			instance.GetComponent<SyncOwner> ().setOwner (player);
 			//instance.GetComponent<SyncColor> ().myColor = c;
-			m.GainMoney (-ti.cost);
-			h.TakeDamage ((int)ti.damagePerHit);
+			playerId.GainMoney (-towerInfo.cost);
+			playerId.TakeDamage ((int)towerInfo.damagePerHit);
+
 			NetworkServer.SpawnWithClientAuthority (instance, base.connectionToClient);
+
+			msg = TowerCase.PushConfirm;
+		} 
+		else 
+		{
+			msg = TowerCase.RejectMoney;
 		}
+
+		RpcTowerBase (msg);
 	}
-		
+
 }
