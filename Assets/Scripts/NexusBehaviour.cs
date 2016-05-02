d3e40c731a651cbebc3902f633e9c273f859081e
Daniel Martínez <dmc00024@red.ujaen.es>
2016-05-02 20:48:18 +0200

    SaveFaster

--- Assets/Scripts/NexusBehaviour.cs
+++ Assets/Scripts/NexusBehaviour.cs
@@ -14,30 +14,31 @@ public class NexusBehaviour : NetworkBehaviour {
 	
 	}
 
-	void OnTriggerEnter(Collider other) 
+	void OnTriggerEnter(Collider other)
 	{
 		if (!isServer)
 			return;
-		
-		print ("Enter enter");
+		if (other.tag == GameObject.FindGameObjectWithTag ("enemigo")) {
+			print ("Enter enter");
 
-		GameObject A = (((NetworkMan)NetworkMan.singleton).A),
-				   B = (((NetworkMan)NetworkMan.singleton).B);
-		PlayerId playerId = null;
+			GameObject A = (((NetworkMan)NetworkMan.singleton).A),
+			B = (((NetworkMan)NetworkMan.singleton).B);
+			PlayerId playerId = null;
 
-		/*UnitInfo unit = other.GetComponent<UnitInfo> ();
-		SyncOwner syncOwner = other.GetComponent<SyncOwner> ();
+			/*UnitInfo unit = other.GetComponent<UnitInfo> ();
+			SyncOwner syncOwner = other.GetComponent<SyncOwner> ();
 
-		if (syncOwner.getOwner () != A) 
-		{
-			playerId = A.GetComponent<PlayerId> ();
-		} 
-		else 
-		{
-			playerId = B.GetComponent<PlayerId> ();
-		} */
+			if (syncOwner.getOwner () != A) 
+			{
+				playerId = A.GetComponent<PlayerId> ();
+			} 
+			else 
+			{
+				playerId = B.GetComponent<PlayerId> ();
+			} */
 
-		//playerId.TakeDamage (unit.damage);
-		NetworkServer.UnSpawn (other.gameObject);
+			//playerId.TakeDamage (unit.damage);
+			Destroy (other.gameObject);
+		}
 	}
 }
