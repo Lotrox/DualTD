b006c0a814f3dcbdb419c110bd33d67886957850
José Ángel Pastrana Padilla <japp0005@red.ujaen.es>
2016-05-03 22:45:37 +0200

    No se admiten más de dos jugadores, sorry.

--- Assets/Scripts/Network/NetworkMan.cs
+++ Assets/Scripts/Network/NetworkMan.cs
@@ -16,6 +16,7 @@ public class NetworkMan : NetworkManager {
 	private NetworkConnection Ac, Bc;
 	public int unitsAlive = 0; // Unidades vivas en una oleada.
 	bool firstWave = true;
+	bool init = false;
 
 	// https://github.com/fholm/unityassets/blob/master/VoiceChat/Assets/VoiceChat/Scripts/Demo/HLAPI/VoiceChatNetworkManager.cs
 	// http://docs.unity3d.com/Manual/UNetPlayers.html (OnServerAddPlayer)
@@ -24,7 +25,7 @@ public class NetworkMan : NetworkManager {
 	public override void OnServerDisconnect(NetworkConnection conn)
 	{
 		base.OnServerDisconnect (conn);
-		--count;
+		count = int.MinValue;
 		if (Ac == conn)
 			B.GetComponent<NetworkRpc> ().RpcWinByDisconnection ();
 		if (Bc == conn)
@@ -35,25 +36,29 @@ public class NetworkMan : NetworkManager {
 
 	public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
 	{
-		GameObject player = (GameObject)Instantiate(playerPrefab, Vector3.zero, Quaternion.identity);
-		if (count < 2) 
+		GameObject player = null;
+		if (!init)
 		{
-			player.GetComponent<PlayerId> ().setAttributes (count, new Color (Random.Range (0.0f, 1.0f), Random.Range (0.0f, 1.0f), Random.Range (0.0f, 1.0f), 1.0f));
-			NetworkServer.AddPlayerForConnection (conn, player, playerControllerId);
+			player = (GameObject)Instantiate (playerPrefab, Vector3.zero, Quaternion.identity);
+			if (count < 2)
+			{
+				player.GetComponent<PlayerId> ().setAttributes (count, new Color (Random.Range (0.0f, 1.0f), Random.Range (0.0f, 1.0f), Random.Range (0.0f, 1.0f), 1.0f));
+				NetworkServer.AddPlayerForConnection (conn, player, playerControllerId);
 
-			if (count == 0) 
-			{
-				A = player;
-				Ac = conn;
-			} 
-			else 
-			{
-				B = player;
-				Bc = conn;
-				waveTime = globalTime = Time.realtimeSinceStartup;
-				print ("Ha comenzado la partida");
-				A.GetComponent<NetworkRpc> ().RpcStandby ();
-				B.GetComponent<NetworkRpc> ().RpcStandby ();
+				if (count == 0)
+				{
+					A = player;
+					Ac = conn;
+				} else
+				{
+					B = player;
+					Bc = conn;
+					init = true;
+					waveTime = globalTime = Time.realtimeSinceStartup;
+					print ("Ha comenzado la partida");
+					A.GetComponent<NetworkRpc> ().RpcStandby ();
+					B.GetComponent<NetworkRpc> ().RpcStandby ();
+				}
 			}
 		}
 		++count;
@@ -61,23 +66,23 @@ public class NetworkMan : NetworkManager {
 
 	void Update()
 	{
-		if (count >= 2) // Los 2 jugadores están en partida.
-		{
-			if (firstWave) 
+		if (count >= 2)
+		{ // Los 2 jugadores están en partida.
+			if (firstWave)
 			{
 				firstWave = !firstWave;
 				A.GetComponent<PlayerId> ().GainMoney (50);
 				B.GetComponent<PlayerId> ().GainMoney (50);
 			}
-				
+			
 			if (!waveSpawned) 
 			{
-				if (Time.realtimeSinceStartup >= waveTime + 20.0f)
+				if (Time.realtimeSinceStartup >= waveTime + 20.0f) 
 				{
 					spawnUnits ();
 					waveSpawned = true;
 				}
-			}
+			} 
 			else if (waveFinished ()) 
 			{
 				waveTime = Time.realtimeSinceStartup;
@@ -86,8 +91,7 @@ public class NetworkMan : NetworkManager {
 				A.GetComponent<NetworkRpc> ().RpcStandby ();
 				B.GetComponent<NetworkRpc> ().RpcStandby ();
 			}
-		}
-		else 
+		} else 
 		{
 			// Falta algún jugador.
 		}
