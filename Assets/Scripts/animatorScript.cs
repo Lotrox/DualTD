0197cc7ce253e0e772029bc8c7c4bc9f7dd2d43d
Daniel Martínez <dmc00024@red.ujaen.es>
2016-05-04 23:57:02 +0200

    Modelos en 3D importados. Animation controller

---
+++ Assets/Scripts/animatorScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class animatorScript : MonoBehaviour {
+
+	public RuntimeAnimatorController ani;
+
+	// Use this for initialization
+	void Start () {
+		gameObject.GetComponentInChildren<Animator> ().runtimeAnimatorController = ani;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
